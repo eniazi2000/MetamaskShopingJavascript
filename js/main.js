c14b2b39ca8fdd0411711bceb4f189aba6c7dd39
eniazi2000 <e.niazi.2000@gmail.com>
2022-10-24 14:46:47 +0330

    Update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-const ethereumButton = document.querySelector(".enableEthereumButton");
-const sendEthButton = document.querySelector(".sendEthButton");
-const sumLabel = document.getElementById("sumLabel");
+var ethereumButton = $(".enableEthereumButton");
+const sendEthButton = $(".sendEthButton");
 
-const showAccount = document.querySelector(".showAccount");
-const AccountBalance = document.querySelector(".AccountBalance");
+var newchainId;
+const showAccount = $(".showAccount");
+var AccountBalance =$(".AccountBalance");
 
 let accounts = [];
 
@@ -11,124 +11,93 @@ if (typeof web3 !== "undefined") {
   web3 = new Web3(web3.currentProvider);
 } else {
   // set the provider you want from Web3.providers
-  // web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-  web3 = new Web3(
-    new Web3.providers.HttpProvider(
-      "https://rinkeby.infura.io/v3/62221142f20f4b40a7a2055db10b845e"
-    )
-  );
+  //web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    web3 = new Web3(
+      new Web3.providers.HttpProvider(
+        "https://rpc.ankr.com/polygon_mumbai"
+      )
+    );
 }
-// web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+//web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 web3 = new Web3(
   new Web3.providers.HttpProvider(
-    "https://rinkeby.infura.io/v3/62221142f20f4b40a7a2055db10b845e"
+    "https://rpc.ankr.com/polygon_mumbai"
   )
 );
+
+
+const chainId = web3.utils.toHex(80001);
 //Sending Ethereum to an address
 
-if (sendEthButton) {
-  sendEthButton.addEventListener("click", () => {
-    ethereum
-      .request({
-        method: "eth_sendTransaction",
-        params: [
-          {
-            from: accounts[0],
-            to: "0xd8C50526A2C28DAB4ACAa3F662827b34B9577d6e",
-            value: web3.utils.toHex(web3.utils.toWei(sumLabel.innerText)),
-            gasPrice: web3.utils.toHex(21000),
-            gas: web3.utils.toHex(21000),
-           // chainId: 0x4
-          },
-        ],
-      })
-      .then((txHash) => {console.log(txHash)
-    createAlert("success","buy is successfully ,  your transaction id :"+txHash);
-    })
-      .catch((error) => {
-        console.log(error.code);
-        createAlert("danger","transaction is get error ,  error code :"+error.code+",message : "+error.message);
-      });
-  });
+
+
+function checkMetaMask() {
+  const metamaskIcon = $(".mmi");
+  const messageLabel = $("#messageLabel");
+  if (!window.ethereum) {
+
+    $(metamaskIcon).attr("src", "img/metamaskd.png");
+    if (messageLabel)
+      $(messageLabel).html("MetaMAsk is not installed pleas install it and reload this page.");
+    return false
+  }
+  else {
+    $(metamaskIcon).attr("src", "img/metamask.png");
+
+    if (messageLabel)
+      $(messageLabel).html("MetaMAsk is installed.");
+  }
+}
+
+function createAlert(type, message) {
+  var str = '<div class="alert alert-' + type + '" role="alert">' + message + '</div>';
+  $("#messageHolder").html(str);
 }
 
 
-function checkMetaMask()
-{
-    const metamaskIcon = document.querySelector(".mmi");
-    const messageLabel = document.getElementById("messageLabel");
-    if (!window.ethereum) {
-
-        metamaskIcon.setAttribute("src" ,"img/metamaskd.png");
-        if(messageLabel)
-        messageLabel.innerHTML = "MetaMAsk is not installed pleas install it and reload this page.";
-        return false
-    }
-    else
-    {
-        metamaskIcon.setAttribute("src" ,"img/metamask.png");
-        if(messageLabel)
-        messageLabel.innerHTML = "MetaMAsk is installed.";
-    }
-}
-
-function createAlert(type,message)
-{
-    var str='<div class="alert alert-'+type+'" role="alert">'+message+'</div>';
-    $("#messageHolder").html(str);
-}
-
-ethereumButton.addEventListener("click", () => {
-  getAccount();
-});
 
 
 ethereum.on('chainChanged', handleChainChanged);
 
 function handleChainChanged(_chainId) {
   // We recommend reloading the page, unless you must do otherwise
-  window.location.reload();
+  //window.location.reload();
+  newchainId = _chainId;
 }
 
 
 
 async function getAccount() {
-  const chainId = await ethereum.request({ method: 'eth_chainId' });
-  
-if(chainId!=0x4)
-{
-createAlert("danger","you are not connect to rinkbey network ,please change your metamask network to rinkbey");  
-return;
-}
-
-    try
-    {
-  accounts = await ethereum.request({ method: "eth_requestAccounts" });
-  
-
-  const account = accounts[0];
-  if (showAccount) {
-    showAccount.innerHTML = account;
-    web3.eth
-      .getBalance(account)
-      .then(function (result) {
-        console.log("Balance : ", web3.utils.fromWei(result, "ether"));
-        AccountBalance.innerHTML = web3.utils.fromWei(result, "ether");
-      })
-      .catch((error) => {
-        console.log(error);
-       
-        createAlert("warning","Can't read balance of wallet.");
-      });
+   newchainId = await ethereum.request({ method: 'eth_chainId' });
+  if (newchainId != chainId) {
+    createAlert("danger", "you are not connect to chain id network ,please change your metamask network to rinkbey");
+    return;
   }
-  else
-  {
-      location.href="../index.html";
+
+  try {
+    accounts = await ethereum.request({ method: "eth_requestAccounts" });
+    const account = accounts[0];
+    console.log(account);
+    if (showAccount) {
+      $(".showAccount").html(account);
+      web3.eth
+        .getBalance(account)
+        .then(function (result) {
+          console.log("Balance : ", web3.utils.fromWei(result, "ether"));
+          $(".AccountBalance").html( web3.utils.fromWei(result, "ether"));
+        })
+        .catch((error) => {
+          console.log(error);
+
+          createAlert("warning", "Can't read balance of wallet.");
+        });
+    }
+    else {
+     // location.href = "../index.html";
+    }
   }
-}
-catch(error)
-{
- console.log(error);
- location.href="../login.html";
-}
+  catch (error) {
+    console.log(error);
+    //location.href = "../login.html";
+  }
 }
